2f570373a14dfdc7d1cd1dcedf6465e8d5110644
clibc <shirvani.abddullayev@gmail.com>
2022-05-30 13:55:05 +0400

    next-error thing

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 pushd _build
-      cl /nologo /EHsc /Zi ..\main.cpp /link user32.lib OpenGL32.lib Gdi32.lib && main.exe
+      cl /FC /nologo /EHsc /Zi ..\main.cpp /link user32.lib OpenGL32.lib Gdi32.lib && main.exe
 popd
