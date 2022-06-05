c8de50b10327e71c38360c6a9af5265e5a913e6d
clibc <shirvani.abddullayev@gmail.com>
2022-06-06 00:35:45 +0400

    warnings and batching

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 pushd _build
-      REM 4244: type conversion 
       REM 4100: unreferenced formal parameter
       REM 4505: unreferenced formal function
       cl /Od /D_CRT_SECURE_NO_WARNINGS /FC /nologo /EHsc /Zi /W4 /WX /wd4100 /wd4505 ..\main.cpp /link user32.lib OpenGL32.lib Gdi32.lib && main.exe
